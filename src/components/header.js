39e8060e099256e510dffc6d3172b9a564536c23
Jiiker <motive46@naver.com>
2023-10-01 21:24:15 +0900

    header 만들기

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./header.css";
+import Carticon from "../icons/cart.svg";
+import Usericon from "../icons/person.svg";
+import Loginicon from "../icons/indent.svg";
 
 export const Header = () => {
-  return <div></div>;
+  return (
+    <div className='header'>
+      <div className='title'>Shop</div>
+      <div className='icons'>
+        <img src={Carticon} />
+        <img src={Usericon} />
+        <img src={Loginicon} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
